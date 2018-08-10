ad022427df91419acbfb807e1a6f247ea13ee014
julian <julian.theis@tuhh.de>
2018-08-10 17:03:04 +0200

    BugFix in Hinf Design

--- Software/Geri_Model_Package/HinfControlDesign/DESIGN2_RobustFlutterSuppression-18-08-03.m
+++ Software/Geri_Model_Package/HinfControlDesign/DESIGN2_RobustFlutterSuppression-18-08-03.m
@@ -134,7 +134,7 @@ Weta = 1*blkdiag(1/20,1/20);   %*100 *100
 % Select disturbance weight
 Dd = Du; %Du; %this could be a dedicated Dd different from Du
 
-[syn_mod,~,~,~,st] = modalform( DesignModel(:,:,VV==designspeed) );
+syn_mod = modalform( DesignModel(:,:,VV==designspeed) );
 
 %provide block structure
 st = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,[18 19],[20 21],[22, 23],[24 25],[26 27],[28 29],[30 31],[32 33],[34 35],[36 37],[38 39],[40 41],[42 43]};
